375d3e532d33fa3e85cc9719ca39dd57975ca309
rohitchouhan3512 <rohitchouhan3512@gmail.com>
2023-09-09 21:41:50 +0530

    defined messsage body

---
+++ src/main/java/com/rohitchouhan35/wchat/chat/ChatController.java
@@ -0,0 +1,17 @@
+package com.rohitchouhan35.wchat.chat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+}
---
+++ src/main/java/com/rohitchouhan35/wchat/chat/MessageType.java
@@ -0,0 +1,9 @@
+package com.rohitchouhan35.wchat.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVER
+
+}
