cba39a76c6f11871c85ca497d128b136a6fbb938
rohitchouhan3512 <rohitchouhan3512@gmail.com>
2023-09-09 18:08:32 +0530

    initial commit

---
+++ src/main/java/com/rohitchouhan35/wchat/WChatApplication.java
@@ -0,0 +1,13 @@
+package com.rohitchouhan35.wchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WChatApplication.class, args);
+	}
+
+}
